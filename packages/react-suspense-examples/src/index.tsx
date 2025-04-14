59478b892828eb2560ad1cc6e4fe9af7c6217c41
vikr01 <vik120@icloud.com>
2025-04-14 11:47:35 -0700

    monkeypatch React.createElement to see the current owner

--- packages/react-suspense-examples/src/index.tsx
+++ packages/react-suspense-examples/src/index.tsx
@@ -6,6 +6,31 @@ import reportWebVitals from './reportWebVitals';
 
 /* @ts-expect-error */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const {
+  __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED: oldReactInternals,
+  __CLIENT_INTERNALS_DO_NOT_USE_OR_WARN_USERS_THEY_CANNOT_UPGRADE:
+    newReactInternals,
+} = React as any
+
+const oldCreateElement = React.createElement;
+type CreateElementParams = Parameters<typeof React.createElement>;
+
+// @ts-expect-error(2540) -- React.createElement should be used as readonly, but in this case it's fine because we want it to always set a context
+React.createElement = /* prevent lint from moving function to same row as assignment due to ts-expect-error */
+function (...args: CreateElementParams) {
+  try {
+    const owner = newReactInternals?.A?.getOwner?.();
+
+    console.log('owner', owner);
+  }
+  catch(err) {
+    console.error(err);
+  }
+
+  return oldCreateElement(...args);
+}
+
 root.render(
   <React.StrictMode>
     <App />
