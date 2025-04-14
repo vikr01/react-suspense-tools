9dedfd6c5ab223117f6fbdac21e426b1b4ed06f8
vikr01 <vik120@icloud.com>
2025-04-13 18:39:52 -0700

    Migrated some js files to typescript in example package

--- packages/react-suspense-examples/src/components/RandomNumberPacket.tsx
+++ packages/react-suspense-examples/src/components/RandomNumberPacket.tsx
@@ -1,14 +1,20 @@
 import {use} from 'react';
 import * as React from 'react';
+/* @ts-expect-error */
 import getRandomNumberPacket from "../api/getRandomNumberPacket";
 import { JsonView, allExpanded} from 'react-json-view-lite';
+/* @ts-expect-error */
 import useSuspenseRef from '../hooks/useSuspenseRef';
 
 const promise = getRandomNumberPacket(5);
 
-export default function RandomNumberPacket({seed}) {
+type Props = {
+    seed: string,
+};
+
+export default function RandomNumberPacket({seed}: Props): React.ReactElement {
     useSuspenseRef('foo');
     // const randomNumberPacket = use(promise);
     
-    return <JsonView data={null} shouldExpandNode={allExpanded}/>
+    return <JsonView data={{}} shouldExpandNode={allExpanded}/>
 }
--- packages/react-suspense-examples/src/index.tsx
+++ packages/react-suspense-examples/src/index.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
+/* @ts-expect-error */
 import reportWebVitals from './reportWebVitals';
 
+/* @ts-expect-error */
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
