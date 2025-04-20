af877136fa0504b98642a73d7c9eda68370d6ded
vikr01 <vik120@icloud.com>
2025-04-20 14:56:02 -0700

    Update more tests for useStructuralId
    Now, we're mocking the randomKey React makes to assign to fiber nodes, so that we can directly access the fiber nodes via refs.
    
    We can then test those fiber nodes to make sure the stopNode is what we expect.

--- packages/useStructuralId/src/__tests__/useStructuralId.test.tsx
+++ packages/useStructuralId/src/__tests__/useStructuralId.test.tsx
@@ -1,6 +1,9 @@
+// This is to mock the reactFiberKey that React makes so we can know what it is. We need to do this before anything react is imported.
+jest.spyOn(global.Math, 'random').mockReturnValue(0.5);
+
 import * as React from 'react';
-import { renderHook, type RenderResult } from "@testing-library/react";
-import useStructuralId, { type Fiber, type Selector } from "../useStructuralId";
+import { renderHook } from "@testing-library/react";
+import useStructuralId, { type Fiber } from "../useStructuralId";
 
 type Props = {
     children: React.ReactNode,
@@ -12,20 +15,36 @@ type ComponentMap = {
     [key: string]: TestComponent,
 };
 
+type ReactFiberKey = '_reactFiber$... - random key here, this is just for typescript to properly catch the type';
+
+type ReactFiberHTMLElement = HTMLElement & {[key in ReactFiberKey]: Fiber};
+
+// See here for how react generates this: https://github.com/facebook/react/blob/bc6184dd993e6ea0efdee7553293676db774c3ca/packages/react-dom-bindings/src/client/ReactDOMComponentTree.js#L40
+// @ts-expect-error We're forcing the type to _reactFiber since the rest of the string is generated at runtime
+const reactFiberKey: ReactFiberKey = `__reactFiber$${Math.random().toString(36).slice(2)}`;
+
+function assertNonNull<T>(x: T): asserts x is NonNullable<T> {
+    expect(x).not.toBeNull();
+}
+
 describe('useStructuralId', () => {
     let TreeComponent: TestComponent;
     let components: ComponentMap;
 
     beforeAll(()=>{
         [TreeComponent, components] = generateTree(5);
-    })
+    });
+
+    afterAll(()=>{
+        jest.spyOn(global.Math, 'random').mockRestore();
+    });
 
     it('should return a structural id', () => {
         const {result} = renderHook(()=>useStructuralId(null, []), {wrapper: TreeComponent});
 
         const [structuralId, stopNode] = result.current;
 
-        expect(structuralId).toMatchSnapshot();
+        expect(structuralId).toMatchInlineSnapshot(`"0:TestComponent:n:0,Component5:n:0,Component4:n:0,Component3:n:0,Component2:n:0,Component1:n:0,Unknown:n:0"`);
 
         expect(stopNode).toBeNull();
     });
@@ -38,33 +57,46 @@ describe('useStructuralId', () => {
 
         const [structuralId, stopNode] = result.current;
 
-        expect(structuralId).toMatchSnapshot();
+        expect(structuralId).toMatchInlineSnapshot(`"0:TestComponent:n:0,Component5:n:0,Component4:n:0,Component3:n:0"`);
 
         expect(stopNode?.elementType).toBe(components.Component3);
     });
 
     it('should work with html tags', ()=>{
         const OldComponent3 = components.Component3;
-        const htmlNodeRef: React.RefObject<HTMLDivElement | null> = {
+
+        const htmlNodeRef: React.RefObject<ReactFiberHTMLElement | null> = {
             current: null,
         };
 
-        const DivComponent = ({children}: Props)=>(
-            <div data-testid="FakeComponent3" ref={htmlNodeRef}>
-                <OldComponent3>
-                    {children}
-                </OldComponent3>
-            </div>
-        );
+        const DivComponent = jest.fn(({children}: Props)=>{
+            return (
+                <span data-testid="FakeComponent3" ref={htmlNodeRef}>
+                    <OldComponent3>
+                        {children}
+                    </OldComponent3>
+                </span>
+            );
+        });
+
         components.Component3 = DivComponent;
 
         const {result} = renderHook(
-            ()=>useStructuralId((node: Fiber)=>node.elementType === 'div', []),
+            ()=>useStructuralId((node: Fiber)=>node.elementType === 'span', []),
             {wrapper: TreeComponent}
         );
 
+        expect(DivComponent).toHaveBeenCalledTimes(1);
+
         const [structuralId, stopNode] = result.current;
 
+        expect(structuralId.split(',')).toHaveLength(5);
+
+        assertNonNull(htmlNodeRef.current);
+
+        expect(stopNode).toBe(htmlNodeRef.current[reactFiberKey]);
+
+        components.Component3 = OldComponent3;
     });
 
 });
@@ -84,11 +116,11 @@ function generateTree(count: number): [TestComponent, ComponentMap] {
   
     for (let i = count-1; i >= 1; i--) {
         const name = `Component${i}`;
-
-        const InnerComponent = components[`Component${i+1}`];
         
         const Comp: TestComponent = ({
             [name]({children, ...props}: Props) {
+                const InnerComponent = components[`Component${i+1}`];
+
                 return (
                     <InnerComponent {...props}>
                         {children}
@@ -96,6 +128,7 @@ function generateTree(count: number): [TestComponent, ComponentMap] {
                 );
             }
         })[name];
+
         components[name] = Comp;
         PrevComponent = Comp;
     }
