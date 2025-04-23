b73d004139405f647794d3d983a6d8e8bdc57d45
vikr01 <vik120@icloud.com>
2025-04-23 13:48:16 -0700

    add utils for our own error boundary to force an error and unset the error state

---
+++ packages/useSuspenseRef/src/__tests__/utils/RecoverableErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import * as React from "react";
+import { Component } from "react";
+
+type Props<T> = {
+  children: React.ReactNode;
+  fallback: React.ReactNode;
+  resetErrorRef: React.RefObject<T | (() => void)>;
+};
+
+export default class RecoverableErrorBoundary<T> extends Component<Props<T>> {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(err: unknown) {
+    let res = false;
+    if (err != null) {
+      res = true;
+      console.log("got error", err);
+    }
+
+    return { hasError: res };
+  }
+
+  render() {
+    this.props.resetErrorRef.current = () => {
+      this.setState({ hasError: false });
+    };
+
+    if (this.state.hasError) {
+      return this.props.fallback;
+    }
+
+    return this.props.children;
+  }
+}
