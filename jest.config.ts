51b9a40130ea0a5930796a6d681030bc049d9bf6
vikr01 <vik120@icloud.com>
2025-04-25 19:25:47 -0700

    move jest config to ts

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,3 @@
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-
 export default {
   preset: "ts-jest",
   testEnvironment: "jsdom", // or 'node' if no DOM is needed
