5a6ebdf487ce3097edd82b4128c51e02f5801cf7
vikr01 <vik120@icloud.com>
2025-04-26 01:01:44 -0700

    add types for each jest config

--- packages/useStructuralId/jest.config.ts
+++ packages/useStructuralId/jest.config.ts
@@ -1,5 +1,8 @@
+import type { Config } from "@jest/types";
 import baseConfig from "../../jest.config.base";
 
-export default {
+const config: Config.InitialOptions = {
   ...baseConfig,
 };
+
+export default config;
