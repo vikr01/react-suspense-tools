88314579f0989f813275fa182a1e1e0fa1ba5886
vikr01 <vik120@icloud.com>
2025-04-26 19:58:06 -0700

    modify API for useSuspendable to allow for a dependencies array

--- packages/useSuspendable/src/useSuspendable.ts
+++ packages/useSuspendable/src/useSuspendable.ts
@@ -1,31 +1,11 @@
-import { useCallback } from "react";
-import useForceUpdate from "use-force-update";
 import useSuspenseRef from "use-suspense-ref";
+import buildUseStable from "react-use-stable/builder";
 
-const SENTINEL: unique symbol = Symbol();
+const useStable = buildUseStable(useSuspenseRef);
 
-type Result<T> = {
-  promise: PromiseLike<T>;
-  reset: () => void;
-};
-
-export default function useSuspendable<T>(cb: () => PromiseLike<T>): Result<T> {
-  const forceUpdate = useForceUpdate();
-
-  const suspenseRef = useSuspenseRef<typeof SENTINEL | PromiseLike<T>>(
-    SENTINEL,
-  );
-  if (suspenseRef.current == SENTINEL) {
-    suspenseRef.current = cb();
-  }
-
-  const reset = useCallback(() => {
-    suspenseRef.current = cb();
-    forceUpdate();
-  }, [cb, forceUpdate]);
-
-  return {
-    promise: suspenseRef.current,
-    reset,
-  };
+export default function useSuspendable<T>(
+  cb: () => PromiseLike<T>,
+  dependencies: ReadonlyArray<unknown>,
+): PromiseLike<T> {
+  return useStable(cb, dependencies);
 }
