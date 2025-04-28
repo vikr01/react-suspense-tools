c380353409113c4293df1bfa3a52f09747a3ca81
vikr01 <vik120@icloud.com>
2025-04-27 20:03:41 -0700

    add script for getting package names to their paths

--- scripts/packages.ts
+++ scripts/packages.ts
@@ -1,12 +1,24 @@
 import { workspaces } from "../package.json";
 import {
+  type PackageName,
+  type PackagePath,
   convertWorkspaceGlobToProjects,
+  getPackageName,
   hasPackageJSON,
   isJestProject,
 } from "./package-filterers";
+
+type PackageMap = Record<PackageName, PackagePath>;
 
 export const packages = workspaces.map(convertWorkspaceGlobToProjects).flat();
 
 export const packagesWithPackageJson = packages.filter(hasPackageJSON);
 
 export const jestProjects = packages.filter(isJestProject);
+
+export const packageNamesMap: PackageMap = Object.fromEntries(
+  packagesWithPackageJson.map((packagePath) => [
+    getPackageName(packagePath),
+    packagePath,
+  ]),
+);
