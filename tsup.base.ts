5e0cbff804155e07ece8dc8d7f2019b8c648f90a
vikr01 <vik120@icloud.com>
2025-05-06 23:06:28 -0700

    stop codesplitting, caching for turborepo

--- tsup.base.ts
+++ tsup.base.ts
@@ -12,6 +12,8 @@ export const tsupBaseConfig: Options = {
   format: ['esm', 'cjs'],
   target: 'es2020',
   sourcemap: false,
+  splitting: false,
+  minify: false,
   define: {
     'process.env.NODE_ENV': JSON.stringify('production'),
     '__DEV_STRUCTURAL_ID_DEBUG__': JSON.stringify(false),
