e11dc68147b553e6f12f8852c37508946521768c
vikr01 <vik120@icloud.com>
2025-05-09 00:13:34 -0700

    migrate to shared eslint config

--- packages/configs/eslint.config.ts
+++ packages/configs/eslint.config.ts
@@ -1,99 +1,40 @@
-import type { ConfigWithExtendsArray } from "@eslint/config-helpers";
+import { createConfig } from "@vikr01/eslint-config";
 import { defineConfig, globalIgnores } from "eslint/config";
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import tsPlugin from "@typescript-eslint/eslint-plugin";
-import eslintConfigPrettier from "eslint-config-prettier/flat";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-// import { fileURLToPath } from "url";
-import path, {
-  // dirname,
-  relative,
-} from "path";
+import { join as pathJoin } from "path";
 import globals from "globals";
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
 import { packageNamesMap } from "../../scripts/packages";
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-const tsRulesOff: Record<keyof typeof tsPlugin.rules, "off"> =
-  Object.fromEntries(
-    Object.keys(tsPlugin.rules).map((ruleName) => [
-      `@typescript-eslint/${ruleName}`,
-      "off",
-    ]),
-  );
-
-const eslintConfigFilePath = relative(__dirname, __filename);
 const tsConfigPath = require.resolve("../../tsconfig.source.json");
 
 export default defineConfig(
-  tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommended,
-    eslintConfigPrettier,
-    eslintPluginPrettierRecommended,
-    {
-      // all typescript and javascript files
-      files: ["*.ts", "*.tsx", "*.js", "*.jsx"],
-      languageOptions: {
-        parserOptions: {
-          project: [tsConfigPath],
-        },
+  createConfig({ json: false, typescript: true, tsConfigPath }),
+  {
+    // browser files
+    files: [
+      pathJoin(
+        packageNamesMap["react-suspense-examples-vite"],
+        "./**/*.{ts,tsx,js,jsx}",
+      ),
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
       },
     },
-    {
-      // commonjs files only
-      files: ["**/*.cjs"],
-      languageOptions: {
-        globals: {
-          ...globals.node,
-        },
-        sourceType: "commonjs",
-      },
-      rules: {
-        ...tsRulesOff,
-      },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
     },
-    {
-      // browser files
-      files: [
-        path.join(
-          packageNamesMap["react-suspense-examples-vite"],
-          "./**/*.{ts,tsx,js,jsx}",
-        ),
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
       ],
-      languageOptions: {
-        ecmaVersion: 2020,
-        globals: {
-          ...globals.browser,
-        },
-      },
-      plugins: {
-        "react-hooks": reactHooks,
-        "react-refresh": reactRefresh,
-      },
-      rules: {
-        ...reactHooks.configs.recommended.rules,
-        "react-refresh/only-export-components": [
-          "warn",
-          { allowConstantExport: true },
-        ],
-      },
     },
-    {
-      // this eslint config
-      files: [eslintConfigFilePath],
-      languageOptions: {
-        globals: {
-          ...globals.node,
-        },
-      },
-    },
-    globalIgnores(["**/node_modules/", "**/build/"]),
-  ) as ConfigWithExtendsArray,
+  },
+  globalIgnores(["packages/*/build/"]),
 );
