953c054aa36c256ff4c5c6c6b961265e762e0267
vikr01 <vik120@icloud.com>
2025-05-09 04:29:09 -0700

    update shared eslint config to support browser

--- packages/configs/eslint.config.ts
+++ packages/configs/eslint.config.ts
@@ -1,40 +1,24 @@
 import { createConfig } from "@vikr01/eslint-config";
 import { defineConfig, globalIgnores } from "eslint/config";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
 import { join as pathJoin } from "path";
-import globals from "globals";
 import { packageNamesMap } from "../../scripts/packages";
 
 const tsConfigPath = require.resolve("../../tsconfig.source.json");
 
 export default defineConfig(
-  createConfig({ json: false, typescript: true, tsConfigPath }),
-  {
-    // browser files
-    files: [
-      pathJoin(
-        packageNamesMap["react-suspense-examples-vite"],
-        "./**/*.{ts,tsx,js,jsx}",
-      ),
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: {
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
+  createConfig({
+    browser: {
+      files: [
+        pathJoin(
+          packageNamesMap["react-suspense-examples-vite"],
+          "./**/*.{ts,tsx,js,jsx}",
+        ),
       ],
+      react: true,
     },
-  },
+    json: false,
+    typescript: true,
+    tsConfigPath,
+  }),
   globalIgnores(["packages/*/build/"]),
 );
